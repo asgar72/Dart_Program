35362b7852d71b614c0374c4c1c79192c9ce3e9d
asgar72 <72asgar72@gmail.com>
2023-07-04 12:51:39 +0530

    add arrow programmes

---
+++ functions/Arrow_functions/arrow_fn1.dart
@@ -0,0 +1,10 @@
+//Sum of two numbers Without Arrow Function
+void sum (int x, int y) {
+
+// printing the result
+print( 'sum is ${x + y}');
+}
+
+void main (){
+sum(5,2);
+}
---
+++ functions/Arrow_functions/arrow_fn2.dart
@@ -0,0 +1,8 @@
+//Sum of two numbers With Arrow Function
+void main(){
+sum(2,5);
+}
+
+// Arrow Syntax
+void sum(int x,int y) => print('sum is ${ x + y}');
+
